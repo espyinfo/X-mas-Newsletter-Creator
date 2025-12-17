7478e8f3a02b4b4dd7995cfeec2ad860b09407d0
[dyad] <esperienzainformatica@gmail.com>
2025-12-17 18:08:21 +0100

    [dyad] Integrated Vercel Speed Insights - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import NewsletterGenerator from "./pages/NewsletterGenerator";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SpeedInsights />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
